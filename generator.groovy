af62e376e88e743e728136b8eb391681c8fa188d
MoralesBarbera <f.moralesbarbera@accenture.com>
2016-11-11 11:44:05 +0100

    first commit

---
+++ generator.groovy
@@ -0,0 +1,52 @@
+import groovy.io.FileType
+
+def listOfFiles = []
+
+//Separate folders
+def separator = "/"
+def folders = pipelinePath.split(separator)
+
+// Create folders for the path
+def folderName
+def firstFolder = true
+folders.each{ dir ->
+	if(firstFolder){
+		folderName = dir
+		folder(folderName)
+		firstFolder = false
+	}else{
+		folderName = folderName + separator + dir
+		folder(folderName)
+	}
+}
+def pipelineFolderName = folderName + separator + pipelineName
+folder(pipelineFolderName)  
+
+// Time to create Job.
+def jobName = pipelineFolderName + separator + pipelineName
+pipelineJob(jobName) {
+    definition {
+        cpsScm {
+			scm{
+				git{
+					branch("*/develop")
+					remote{
+						url("ssh://git@192.168.99.100:7999/ci/jenkins-files.git")
+						credentials("2abfdf7f-4945-4d12-b760-e1e1da6dc8a7")	
+					}
+				}
+			}
+            scriptPath("${pipelineType}/jenkinsfile")
+        }
+    }
+}
+
+//Generate properties file
+new File("/var/jenkins_home/workspace/${pipelineFolderName}/properties").mkdirs()  
+def propsFile = new File("/var/jenkins_home/workspace/${pipelineFolderName}/properties/config.properties")
+propsFile.write("gitURL=${gitURL}\npipelineType=${pipelineType}\npipelineName=${pipelineName}\npipelinePath=${pipelinePath}")
+
+//Run new pipeline
+queue(jobName)
+
+// https://github.com/mtablado/jenkins-docker.git
