50c6d43a2a9702d72153998230a2a044d5a01043
MoralesBarbera <f.moralesbarbera@accenture.com>
2016-11-16 11:06:24 +0100

    added views for the diferents environments

--- generator.groovy
+++ generator.groovy
@@ -6,6 +6,9 @@ def listOfFiles = []
 def separator = "/"
 def folders = pipelinePath.split(separator)
 
+//Environments list
+def envs = ["DEV","PRE","PRO","UAT"]
+
 // Create folders for the path
 def folderName
 def firstFolder = true
@@ -20,32 +23,54 @@ folders.each{ dir ->
 	}
 }
 def pipelineFolderName = folderName + separator + pipelineName
-folder(pipelineFolderName)  
-
-// Time to create Job.
-def jobName = pipelineFolderName + separator + pipelineName
-pipelineJob(jobName) {
-    definition {
-        cpsScm {
-			scm{
-				git{
-					branch("*/develop")
-					remote{
-						url("https://github.com/francescmorales/jenkinsfiles.git")
-					}
-				}
-			}
-            scriptPath("${pipelineType}/jenkinsfile")
-        }
-    }
+folder(pipelineFolderName){
+	primaryView("DEV")
 }
+
+//generate jobName
+def jobName = pipelineFolderName + separator + pipelineName
 
 //Generate properties file
 new File("/var/jenkins_home/workspace/${pipelineFolderName}/properties").mkdirs()  
 def propsFile = new File("/var/jenkins_home/workspace/${pipelineFolderName}/properties/config.properties")
 propsFile.write("gitURL=${gitURL}\npipelineType=${pipelineType}\npipelineName=${pipelineName}\npipelinePath=${pipelinePath}\nslavePath=/home/jenkins/workspace/${jobName}")
 
-//Run new pipeline
-queue(jobName)
-
-// https://github.com/mtablado/jenkins-docker.git
+//Create jobs for diferents envs
+envs.each{ env ->
+  
+  //Create view
+  listView("${pipelineFolderName}/${env}") {
+      jobs {
+        name("${pipelineName}-${env}")
+      }
+      columns {
+          status()
+          weather()
+          name()
+          lastSuccess()
+          lastFailure()
+          lastDuration()
+          buildButton()
+      }
+  }
+  
+  // Time to create Job.
+  pipelineJob("${jobName}-${env}") {
+      definition {
+          cpsScm {
+              scm{
+                  git{
+                      branch("*/develop")
+                      remote{
+                          url("https://github.com/francescmorales/jenkinsfiles.git")
+                      }
+                  }
+              }
+              scriptPath("${pipelineType}/jenkinsfile")
+          }
+      }
+  }
+  
+  //Run new pipeline
+  queue("${jobName}-${env}")
+}
